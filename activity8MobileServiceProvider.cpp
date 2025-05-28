6d1a3ff98861e8cc8bca0dcd33cf23516b005948
Aqila N <aqila.nasiry@gmail.com>
2025-05-28 00:11:24 -0700

    Updated.

--- activity8MobileServiceProvider.cpp
+++ activity8MobileServiceProvider.cpp
@@ -2,7 +2,85 @@
 //  activity8MobileServiceProvider.cpp
 //  C++Projects
 //
-//  Created by Ahmad Farid Hamdard on 5/27/25.
+//  Created by Aqila Nasiry on 2/22/25.
 //
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
 
-#include "activity8MobileServiceProvider.hpp"
+    // Named constants for package prices and data limits
+
+    const double PAC_A_Cost = 39.99;
+
+    const double PACKAGE_A_LIMIT = 4.0;
+
+    const double PACKAGE_A_EXTRA_COST = 10.0;
+    const double PACKAGE_B_COST = 59.99;
+    const double PACKAGE_B_LIMIT = 8.0;
+    const double PACKAGE_B_EXTRA_COST = 5.0;
+    const double PACKAGE_C_COST = 69.99; // Unlimited data
+    // Variables
+    char packageType;
+    double gigabytesUsed;
+    double totalCost = 0.0;
+    // User input
+    cout << "Enter the package type (A, B, or C): ";
+    cin >> packageType;
+    // Validate input
+    if (packageType != 'A' && packageType != 'B' && packageType != 'C' &&
+
+        packageType != 'a' && packageType != 'b' && packageType != 'c') {
+
+        cout << "Invalid package type. Please enter A, B, or C." << endl;
+
+        return 1;
+
+    }
+
+    
+
+    cout << "Enter the number of gigabytes used: ";
+
+    cin >> gigabytesUsed;
+
+    
+
+    // Calculate the total bill based on the package selected
+
+    switch (toupper(packageType)) {
+
+        case 'A':
+
+            totalCost = PAC_A_Cost;
+
+            if (gigabytesUsed > PACKAGE_A_LIMIT) {
+
+                totalCost += (gigabytesUsed - PACKAGE_A_LIMIT) * PACKAGE_A_EXTRA_COST;
+
+            }
+
+            break;
+
+        
+
+        case 'B':
+
+            totalCost = PACKAGE_B_COST;
+
+            if (gigabytesUsed > PACKAGE_B_LIMIT) {
+
+                totalCost += (gigabytesUsed - PACKAGE_B_LIMIT) * PACKAGE_B_EXTRA_COST;
+            }
+
+            break;
+        case 'C':
+            totalCost = PACKAGE_C_COST;
+            break;
+    }
+
+    // Display the total cost
+    cout << fixed << setprecision(2);
+    cout << "Total amount due: $" << totalCost << endl;
+    return 0;
+}
--- activity9ColorMixer.cpp
+++ activity9ColorMixer.cpp
@@ -2,7 +2,42 @@
 //  activity9ColorMixer.cpp
 //  C++Projects
 //
-//  Created by Ahmad Farid Hamdard on 5/27/25.
+//  Created by Aqila Nasiry on 2/25/25.
 //
 
 #include <stdio.h>
+#include <iostream>
+
+#include <string>
+using namespace std;
+int main() {
+
+   string color1, color2;
+    // asking user for input
+
+    cout << "Enter the first color (red, blue, yellow): ";
+    cin >> color1;
+    cout << "Enter the second color (red, blue, yellow): ";
+    cin >> color2;
+
+    if ((color1 != "red" && color1 != "blue" && color1 != "yellow") ||
+
+        (color2 != "red" && color2 != "blue" && color2 != "yellow")) {
+
+        cout << "Invalid color entered. Please enter only primary colors (red, blue, or yellow).\n";
+
+    }
+
+else {
+
+        if ((color1 == "red" && color2 == "blue") || (color1 == "blue" && color2 == "red")) {
+            cout << "Mixing red and blue gives purple.\n";
+        } else if ((color1 == "red" && color2 == "yellow") || (color1 == "yellow" && color2 == "red")) {
+            cout << "Mixing red and yellow gives orange.\n";
+        } else if ((color1 == "blue" && color2 == "yellow") || (color1 == "yellow" && color2 == "blue")) {
+            cout << "Mixing blue and yellow gives green.\n";
+
+        }
+    }
+    return 0;
+}
